6d4ed22999fe4a836d12874e21219613b1161a53
Ahad Birang <farnabaz@gmail.com>
2022-03-04 12:06:34 +0000

    chore: migrate to v2

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,5 @@
-export default {
-    /*
-     ** Deployment target
-     ** Doc: https://nuxtjs.org/guides/features/deployment-targets
-     */
-    target: "static",
-    /*
-     ** Rendering mode
-     ** Doc: https://nuxtjs.org/guides/features/rendering-modes
-     */
-    mode: "universal",
-  
-    /*
-     ** Nuxt.js modules
-     ** Doc: https://nuxtjs.org/guides/configuration-glossary/configuration-modules
-     */
-    modules: ["@nuxt/content"],
-    content: {
-      // Disable for security reason on CodeSandBox
-      liveEdit: false
-    },
-    /*
-     ** Headers of the page
-     ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
-     */
-    head: {
-      title: "Nuxt.js Playground for nuxt/content",
-      meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" }
-      ]
-    },
-  
-    /*
-     ** Plugins to load before mounting the App
-     ** Doc: https://nuxtjs.org/guide/plugins
-     */
-    plugins: []
-  };
-  
+import { defineNuxtConfig } from "nuxt3";
+
+export default defineNuxtConfig({
+  modules: ["@nuxt/content"]
+})
---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Content v-if="content" :id="content.id" />
+    <div v-else>Page not found</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+
+const { data: content } = await useAsyncData('content', () => useContentQuery(route.path).findOne())
+</script>
